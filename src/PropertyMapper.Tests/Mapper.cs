13bfd35f8e3237a5304e9bf92145917a1de8cf4b
Jens Andresen <jad@trifork.com>
2013-12-02 09:25:39 +0100

    moved mapper into seperate file

---
+++ src/PropertyMapper.Tests/Mapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace PropertyMapper.Tests
+{
+  public class Mapper
+  {
+    public static void Map<TSource, TDestination>(TSource source, TDestination destination)
+    {
+      var sourceProperties = GetPropertiesFrom(source);
+      var destinationProperties = GetPropertiesFrom(destination);
+
+      foreach (var destinationProperty in destinationProperties)
+      {
+        var sourceProperty = sourceProperties.SingleOrDefault(info => IsMatch(info, destinationProperty));
+
+        if (sourceProperty != null)
+        {
+          CopyPropertyValue(source, sourceProperty, destination, destinationProperty);
+        }
+      }
+    }
+
+    private static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination, PropertyInfo destinationProperty)
+    {
+      var value = sourceProperty.GetValue(source, null);
+      destinationProperty.SetValue(destination, value, null);
+    }
+
+    private static PropertyInfo[] GetPropertiesFrom(object source)
+    {
+      return source
+        .GetType()
+        .GetProperties()
+        .Where(p => p.GetGetMethod(false) != null)
+        .Where(p => p.GetSetMethod(false) != null)
+        .ToArray();
+    }
+
+    private static bool IsMatch(PropertyInfo first, PropertyInfo second)
+    {
+      return first.PropertyType == second.PropertyType &&
+             first.Name == second.Name;
+    }
+  }
+}
--- src/PropertyMapper.Tests/TestMapper.cs
+++ src/PropertyMapper.Tests/TestMapper.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using NUnit.Framework;
-using System.Linq;
+﻿using NUnit.Framework;
 
 namespace PropertyMapper.Tests
 {
@@ -51,47 +49,6 @@ namespace PropertyMapper.Tests
       public string FirstName { get; set; }
       public string LastName { get; private set; }
       public int Age { get; set; }
-    }
-  }
-
-  public class Mapper
-  {
-    public static void Map<TSource, TDestination>(TSource source, TDestination destination)
-    {
-      var sourceProperties = GetPropertiesFrom(source);
-      var destinationProperties = GetPropertiesFrom(destination);
-
-      foreach (var destinationProperty in destinationProperties)
-      {
-        var sourceProperty = sourceProperties.SingleOrDefault(info => IsMatch(info, destinationProperty));
-
-        if (sourceProperty != null)
-        {
-          CopyPropertyValue(source, sourceProperty, destination, destinationProperty);
-        }
-      }
-    }
-
-    private static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination, PropertyInfo destinationProperty)
-    {
-      var value = sourceProperty.GetValue(source, null);
-      destinationProperty.SetValue(destination, value, null);
-    }
-
-    private static PropertyInfo[] GetPropertiesFrom(object source)
-    {
-      return source
-        .GetType()
-        .GetProperties()
-        .Where(p => p.GetGetMethod(false) != null)
-        .Where(p => p.GetSetMethod(false) != null)
-        .ToArray();
-    }
-
-    private static bool IsMatch(PropertyInfo first, PropertyInfo second)
-    {
-      return first.PropertyType == second.PropertyType &&
-             first.Name == second.Name;
     }
   }
 }
