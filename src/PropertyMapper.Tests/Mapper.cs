d519ceb7681126abb2065bb4b4508733e3a0c281
Jens Andresen <jad@trifork.com>
2013-12-05 09:49:08 +0100

    added caching of type properties

--- src/PropertyMapper.Tests/Mapper.cs
+++ src/PropertyMapper.Tests/Mapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,13 +7,14 @@ namespace PropertyMapper.Tests
 {
     public class Mapper
     {
+        private static readonly IDictionary<Type, PropertyInfo[]> _propertyInfoes = new Dictionary<Type, PropertyInfo[]>();
+
         public static void Map<TSource, TDestination>(TSource source, TDestination destination)
         {
             Map(source, destination, null);
         }
 
-        private static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination,
-                                              PropertyInfo destinationProperty)
+        private static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination, PropertyInfo destinationProperty)
         {
             var value = sourceProperty.GetValue(source, null);
             destinationProperty.SetValue(destination, value, null);
@@ -20,12 +22,22 @@ namespace PropertyMapper.Tests
 
         private static PropertyInfo[] GetPropertiesFrom(object source)
         {
-            return source
-                .GetType()
+            var targetedType = source.GetType();
+
+            if (_propertyInfoes.ContainsKey(targetedType))
+            {
+                return _propertyInfoes[targetedType];
+            }
+
+            var properties = targetedType
                 .GetProperties()
                 .Where(p => p.GetGetMethod(false) != null)
                 .Where(p => p.GetSetMethod(false) != null)
                 .ToArray();
+
+            _propertyInfoes.Add(targetedType, properties);
+
+            return properties;
         }
 
         private static bool IsMatch(PropertyInfo first, PropertyInfo second)
