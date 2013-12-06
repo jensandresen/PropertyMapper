395b36877f255e4b0ae756a7627dd2843615f064
Jens Andresen <jad@trifork.com>
2013-12-06 09:32:31 +0100

    added documentation for public methods

--- src/PropertyMapper.Core/IConfiguration.cs
+++ src/PropertyMapper.Core/IConfiguration.cs
@@ -5,6 +5,11 @@ namespace PropertyMapper
 {
     public interface IConfiguration<TSource, TDestination>
     {
+        /// <summary>
+        /// Specify a property on the destination instance that should be ignored when coping property values.
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="selector">Expression for selecting a property on the destination.</param>
         void Ignore<TValue>(Expression<Func<TDestination, TValue>> selector);
     }
 }
--- src/PropertyMapper.Core/Mapper.cs
+++ src/PropertyMapper.Core/Mapper.cs
@@ -9,9 +9,50 @@ namespace PropertyMapper
     {
         private static readonly IDictionary<Type, PropertyInfo[]> _propertyInfoes = new Dictionary<Type, PropertyInfo[]>();
 
+        /// <summary>
+        /// Copy the value of all properties that has read and write access on both source and destination.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source">The instance to copy property values from.</param>
+        /// <param name="destination">The instance where the property values are copied to.</param>
         public static void Map<TSource, TDestination>(TSource source, TDestination destination)
         {
             Map(source, destination, null);
+        }
+
+        /// <summary>
+        /// Copy the value of all properties that has read and write access on both source and destination. Adjust mapping configuration
+        /// to enable a more customized mapping.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source">The instance to copy property values from.</param>
+        /// <param name="destination">The instance where the property values are copied to.</param>
+        /// <param name="configurator">The configuration instance that enables customization.</param>
+        public static void Map<TSource, TDestination>(TSource source, TDestination destination, Action<IConfiguration<TSource, TDestination>> configurator)
+        {
+            var cfg = new Configuration<TSource, TDestination>();
+            if (configurator != null)
+            {
+                configurator(cfg);
+            }
+
+            var sourceProperties = GetPropertiesFrom(source);
+            var destinationProperties = GetPropertiesFrom(destination);
+
+            foreach (var destinationProperty in destinationProperties)
+            {
+                if (!cfg.ShouldBeIgnored(destinationProperty))
+                {
+                    var sourceProperty = sourceProperties.SingleOrDefault(info => IsMatch(info, destinationProperty));
+
+                    if (sourceProperty != null)
+                    {
+                        CopyPropertyValue(source, sourceProperty, destination, destinationProperty);
+                    }
+                }
+            }
         }
 
         private static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination, PropertyInfo destinationProperty)
@@ -38,31 +79,6 @@ namespace PropertyMapper
         private static bool IsMatch(PropertyInfo first, PropertyInfo second)
         {
             return PropertyHelpers.IsMatch(first, second);
-        }
-
-        public static void Map<TSource, TDestination>(TSource source, TDestination destination, Action<IConfiguration<TSource, TDestination>> configurator)
-        {
-            var cfg = new Configuration<TSource, TDestination>();
-            if (configurator != null)
-            {
-                configurator(cfg);
-            }
-
-            var sourceProperties = GetPropertiesFrom(source);
-            var destinationProperties = GetPropertiesFrom(destination);
-
-            foreach (var destinationProperty in destinationProperties)
-            {
-                if (!cfg.ShouldBeIgnored(destinationProperty))
-                {
-                    var sourceProperty = sourceProperties.SingleOrDefault(info => IsMatch(info, destinationProperty));
-
-                    if (sourceProperty != null)
-                    {
-                        CopyPropertyValue(source, sourceProperty, destination, destinationProperty);
-                    }
-                }
-            }
         }
     }
 }
