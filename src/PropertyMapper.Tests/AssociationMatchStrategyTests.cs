6254aee39be49f70626a7b1949d0eb3eac5f57d6
Jens Andresen <jensandresen@gmail.com>
2013-12-10 22:13:04 +0100

    WIP: association match strategy tests

---
+++ src/PropertyMapper.Tests/AssociationMatchStrategyTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PropertyMapper.Tests.TestDoubles;
+
+namespace PropertyMapper.Tests
+{
+    [TestFixture]
+    public class AssociationMatchStrategyTests
+    {
+        [Test, Ignore]
+        public void can_match_an_association()
+        {
+            var properties = new IProperty[]
+                                 {
+                                     new FakeProperty<string>("BarName")
+                                 };
+
+            var sourceProperty = new FakeProperty<Foo>("Bar");
+            var sut = new AssociationMatchStrategy(properties);
+
+            sut.GetMatchFor(sourceProperty);
+        }
+
+        private class Foo
+        {
+            public Bar Bar { get; set; }
+        }
+
+        private class Bar
+        {
+            public string Name { get; set; }
+        }
+
+        private class Flattened
+        {
+            public string BarName { get; set; }
+        }
+    }
+
+    public class AssociationMatchStrategy : SearchStrategyBase
+    {
+        public AssociationMatchStrategy(IEnumerable<IProperty> properties) : base(properties)
+        {
+
+        }
+
+        public override IProperty GetMatchFor(IProperty destinationProperty)
+        {
+            return null;
+        }
+    }
+}
