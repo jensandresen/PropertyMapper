28e17bd38f6ffc01f4b62f6f24e6f734b09625e9
Jens Andresen <jensandresen@gmail.com>
2013-12-10 21:03:59 +0100

    removed unused code

--- src/PropertyMapper.Core/PropertyHelpers.cs
+++ src/PropertyMapper.Core/PropertyHelpers.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 
 namespace PropertyMapper
 {
     public static class PropertyHelpers
     {
-        public static bool IsMatch(PropertyInfo first, PropertyInfo second)
-        {
-            return first.PropertyType == second.PropertyType &&
-                   first.Name == second.Name;
-        }
-
         public static bool IsMatch(IProperty first, IProperty second)
         {
             return first.Type == second.Type &&
@@ -27,12 +20,6 @@ namespace PropertyMapper
                 .Select(p => new PropertyInfoAdapter(p))
                 .Cast<IProperty>() // todo: remove this!
                 .ToArray();
-        }
-
-        public static void CopyPropertyValue(object source, PropertyInfo sourceProperty, object destination, PropertyInfo destinationProperty)
-        {
-            var value = sourceProperty.GetValue(source, null);
-            destinationProperty.SetValue(destination, value, null);
         }
 
         public static void CopyPropertyValue(object source, IProperty sourceProperty, object destination, IProperty destinationProperty)
